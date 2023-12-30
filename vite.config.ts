19995948b644afc66218f90528997b89c3b5bb49
Johan N. Andersen <jna.jelling+git@gmail.com>
2023-12-30 16:56:08 +0100

    fly.io boykot.fly.dev/api /csv

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
+  // build: { rollupOptions: ['csv-parse/sync']},
   start: {
     middleware: "./src/middleware.ts",
   },
